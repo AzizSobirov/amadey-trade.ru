5319c641a70394ffa3f267b339d7546e1c6fee41
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2024-12-25 11:41:27 +0500

    25.12.2024

---
+++ public/assets/js/app.js
@@ -0,0 +1,46 @@
+const header = document.querySelector(".header");
+if (header) {
+  const triggers = header.querySelectorAll(".header__trigger");
+  const menu = header.querySelector(".header__menu");
+  const services = header.querySelectorAll(".menu-item-has-children");
+  // const servicesSubMenu = services.querySelector(".sub-menu");
+
+  triggers.forEach((trigger) => {
+    trigger.addEventListener("click", () => {
+      menu.classList.toggle("active");
+
+      triggers.forEach((trigger) => {
+        trigger.classList.toggle("active");
+      });
+    });
+  });
+
+  // Close the menu when clicking outside, but only if the menu is open
+  document.addEventListener("click", (event) => {
+    if (menu.classList.contains("active") && !header.contains(event.target)) {
+      // Check if the click is outside the header and the menu is open
+      menu.classList.remove("active");
+      triggers.forEach((trigger) => {
+        trigger.classList.remove("active");
+      });
+    }
+  });
+
+  if (window.innerWidth < 1280) {
+    const icon = document.createElement("span");
+    icon.classList.add("icon-down");
+    icon.innerHTML =
+      '<svg width="9" height="5" viewBox="0 0 9 5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L4.5 4L8 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /></svg>';
+
+    services.forEach((service) => {
+      const link = service.querySelector("a");
+      const subMenu = service.querySelector(".sub-menu");
+
+      link.appendChild(icon);
+      link.addEventListener("click", () => {
+        link.classList.toggle("active");
+        subMenu.classList.toggle("active");
+      });
+    });
+  }
+}
